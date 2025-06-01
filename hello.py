85c3d9e897a8c3b7ce3b9bd5ecd9bc509c294304
abdurku22 <cfgrandmaster@gmail.com>
2025-06-01 15:52:34 +0600

    cpp named

--- hello.py
+++ hello.py
@@ -1,1 +1,36 @@
+#1st vedio
 print("Hello")
+# name ="abdur"
+# price=25.99
+# age=23
+# print("My name is :", name)
+# print("My age is :", age)
+# print(type(age))
+# a=None
+# print(type(a))
+# old= False
+# print(type(old))
+a=12
+b=4 
+# sum=a+b
+# print(sum)
+#arithmatic operator
+# a=3
+# b=8
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+# #to the power
+print(a**b)
+# #Type conversion
+# #Type custing
+# a=3.14
+# # a=str(a)
+name =input("Enter your name:")
+# a=int(input("Enter the value of a"))
+# b=int(input("Enter the value of b"))
+print(name)
+# print("Sum=",a+b)
+# length=len(name)
+# print(length)
